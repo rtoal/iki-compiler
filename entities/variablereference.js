cf4447d0f5070a9092b2afc8e2e4c00668547d48
Ray Toal <rtoal@lmu.edu>
2014-01-03 08:30:27 -0800

    Remove some logging lines that were in there for debugging

--- entities/variablereference.js
+++ entities/variablereference.js
@@ -3,8 +3,6 @@ function VariableReference(token) {
 }
 
 VariableReference.prototype.analyze = function (context) {
-  console.log('context is %j', context)
-  console.log('looking up', this.token.lexeme)
   this.referent = context.lookupVariable(this.token);
   this.type = this.referent.type;
 }
