74177da3d5c862f8a5ac49e9fdc96c8f500088f9
Ray Toal <rtoal@lmu.edu>
2014-01-04 23:00:05 -0800

    Flesh out more tests

--- test/test-scanner.js
+++ test/test-scanner.js
@@ -1,19 +1,25 @@
 var should = require('should');
 var scan = require('../scanner')
+var i = require('util').inspect
 
-describe('Scanner', function() {
+describe('The scanner', function () {
 
-  describe('tokens', function () {
-    it('recognizes a couple of tokens', function (done) {
-      scan('test/hello.iki', function (tokens) {
-        tokens.length.should.eql(2)
-        tokens[0].kind.should.eql('write')
-        tokens[0].lexeme.should.eql('write')
-        tokens[1].kind.should.eql('INTLIT')
-        tokens[1].lexeme.should.eql('0')
-        done();
-      })
+  it('scans the simplest program', function (done) {
+    scan('test/hello.iki', function (tokens) {
+      tokens.length.should.eql(3)
+      i(tokens[0]).should.equal(i({kind:'write',lexeme:'write',line:1,col:1}))
+      i(tokens[1]).should.equal(i({kind:'INTLIT',lexeme:'0',line:1,col:7}))
+      i(tokens[2]).should.equal(i({kind:';',lexeme:';',line:1,col:8}))
+      done()
+    });
+  })
+
+  it('does line numbers', function (done) {
+    scan('test/hello.iki', function (tokens) {
+      tokens[0].line.should.eql(1)
+      tokens[0].col.should.eql(1)
+      done()
     });
   });
-
-})
+  
+});
