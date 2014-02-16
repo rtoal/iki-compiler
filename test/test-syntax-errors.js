00a7f259295da028fe1356b4ce8ecd163258762a
Ray Toal <rtoal@lmu.edu>
2014-02-16 11:17:51 -0800

    Drier tests for syntax error checking

--- test/test-syntax-errors.js
+++ test/test-syntax-errors.js
@@ -3,66 +3,36 @@ var scan = require('../scanner')
 var parse = require('../parser')
 var error = require('../error')
 
-function checkForParseErrors(tokens, done) {
-  count = error.count
-  parse(tokens);
-  (error.count-count).should.be.above(0)
-  done()
+function checkForParseErrors(check, baseFilename) {
+  it(check, function (done) {
+    scan('test/data/syntax-errors/' + baseFilename + '.iki', function (tokens) {
+      var priorErrorCount = error.count
+      parse(tokens);
+      (error.count-priorErrorCount).should.be.above(0)
+      done()
+    })
+  })
 }
 
 describe('The parser', function () {
 
-  it('detects error at empty program', function (done) {
-    scan('test/data/syntax-errors/empty.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
+  var checks = {
+    'detects error at empty program': 'empty',
+    'detects errors at start of statement': 'bad-statement',
+    'detects unknown types': 'bad-type',
+    'detected bad expressions in assignments': 'bad-expr-in-assignment',
+    'detects a missing loop keyword': 'missing-loop',
+    'detects a missing end keyword': 'missing-end',
+    'detects missing commas in read statements': 'no-comma-in-read',
+    'detects missing commas in write statements': 'no-comma-in-write',
+    'detects unbalanced parentheses': 'unbalanced-parens',
+    'detects a missing semicolon after a variable declaration': 'no-semicolon',
+    'detects multiple relational operators without parentheses': 'multiple-relationals'
+  };
 
-  it('detects errors at start of statement', function (done) {
-    scan('test/data/syntax-errors/bad-statement.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
-
-  it('detects bad type errors', function (done) {
-    scan('test/data/syntax-errors/bad-type.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
-
-  it('detects bad expressions in assignments', function (done) {
-    scan('test/data/syntax-errors/bad-expr-in-assignment.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
-
-  it('detects a missing loop keyword', function (done) {
-    scan('test/data/syntax-errors/missing-loop.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
-
-  it('detects a missing end keyword', function (done) {
-    scan('test/data/syntax-errors/missing-end.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
-
-  it('detects missing commas in read statements', function (done) {
-    scan('test/data/syntax-errors/no-comma-in-read.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
-
-  it('detects missing commas in write statements', function (done) {
-    scan('test/data/syntax-errors/no-comma-in-write.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
-
-  it('detects unbalanced parentheses', function (done) {
-    scan('test/data/syntax-errors/unbalanced-parens.iki', function (tokens) {
-      checkForParseErrors(tokens, done)
-    })
-  })
+  for (var check in checks) {
+    if (checks.hasOwnProperty(check)) {
+      checkForParseErrors(check, checks[check])
+    }
+  }
 })
