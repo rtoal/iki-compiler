0a74d2a539ac1c16bf41566ab41c1fca81c14088
Ray Toal <rtoal@lmu.edu>
2014-02-18 23:47:00 -0800

    optimist -> yargs

--- iki.js
+++ iki.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-var argv = require('optimist')
-  .usage('$0 [-t] [-a] [-o] [-i] [--target x86|c|js] filename')
+var argv = require('yargs')
+  .usage('$0 [-t] [-a] [-o] [-i] [--target [x86|c|js]] filename')
   .boolean(['t','a','o','i'])
   .describe('t', 'show tokens after scanning then stop')
   .describe('a', 'show abstract syntax tree after parsing then stop')
@@ -18,13 +18,13 @@ var generate = require('./generator')(argv.target)
 var error = require('./error')
 
 scan(argv._[0], function (tokens) {
-  if (error.count > 0) return;
+  if (error.count > 0) return
   if (argv.t) {
     tokens.forEach(function (t) {console.log(t)})
     return
   }
   var program = parse(tokens)
-  if (error.count > 0) return;
+  if (error.count > 0) return
   if (argv.a) {
     console.log(program.toString())
     return
@@ -33,7 +33,7 @@ scan(argv._[0], function (tokens) {
     program = program.optimize()
   }
   program.analyze()
-  if (error.count > 0) return;
+  if (error.count > 0) return
   if (argv.i) {
     program.showSemanticGraph()
     return
