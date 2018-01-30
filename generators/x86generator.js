4cd747874ce407dcb6328663c18ec9cf53189953
Ray Toal <rtoal@lmu.edu>
2018-01-29 21:52:48 -0800

    Clean up linter errors

--- generators/x86generator.js
+++ generators/x86generator.js
@@ -1,24 +1,22 @@
 const HashMap = require('hashmap').HashMap;
 
-const usedVariables = {};
+const usedVariables = new Set();
 
 module.exports = gen;
 
 const makeVariable = (() => {
-  const lastId = 0;
+  let lastId = 0;
   const map = new HashMap();
   return (v) => {
-    if (!map.has(v)) map.set(v, ++lastId);
-    return '_v' + map.get(v);
-  }
+    if (!map.has(v)) map.set(v, ++lastId); // eslint-disable-line no-plusplus
+    return `_v${map.get(v)}`;
+  };
 })();
 
-const makeLabel = (function () {
-  const labelsGenerated = 0
-  return function () {
-    return 'L' + (++labelsGenerated)
-  }
-}())
+const makeLabel = (() => {
+  let labelsGenerated = 0;
+  return () => `L${++labelsGenerated}`; // eslint-disable-line no-plusplus
+})();
 
 function gen(e) {
   return generator[e.constructor.name](e);
@@ -26,164 +24,165 @@ function gen(e) {
 
 const generator = {
 
-  'Program': function (program) {
-    emit('.globl', '_main')
-    emit('.text')
-    emitLabel('_main')
-    emit('push', '%rbp')
-    gen(program.block)
-    emit('pop', '%rbp')
-    emit('ret')
-    emit('.data')
-    emitLabel('READ')
-    emit('.ascii', '"%d\\0\\0"') // extra 0 for alignment
-    emitLabel('WRITE')
-    emit('.ascii', '"%d\\n\\0"')
-    for (const s in usedVariables) {
-      emitLabel(s)
+  Program(program) {
+    emit('.globl', '_main');
+    emit('.text');
+    emitLabel('_main');
+    emit('push', '%rbp');
+    gen(program.block);
+    emit('pop', '%rbp');
+    emit('ret');
+    emit('.data');
+    emitLabel('READ');
+    emit('.ascii', '"%d\\0\\0"'); // extra 0 for alignment
+    emitLabel('WRITE');
+    emit('.ascii', '"%d\\n\\0"');
+    usedVariables.forEach((s) => {
+      emitLabel(s);
       emit('.quad', '0');
-    }
+    });
   },
 
-  'Block': function (block) {
-    block.statements.forEach(function (statement) {
-      gen(statement)
-      allocator.freeAllRegisters()
-    })
+  Block(block) {
+    block.statements.forEach((statement) => {
+      gen(statement);
+      allocator.freeAllRegisters();
+    });
   },
 
-  'VariableDeclaration': function (v) {
+  VariableDeclaration() {
     // Intentionally empty
   },
 
-  'AssignmentStatement': function (s) {
-    source = gen(s.source)
-    destination = gen(s.target)
+  AssignmentStatement(s) {
+    let source = gen(s.source);
+    const destination = gen(s.target);
     if (source instanceof MemoryOperand && destination instanceof MemoryOperand) {
-      const oldSource = source
-      source = allocator.makeRegisterOperand()
-      emit('mov', oldSource, source)
+      const oldSource = source;
+      source = allocator.makeRegisterOperand();
+      emit('mov', oldSource, source);
     }
-    emit('mov', source, destination)
+    emit('mov', source, destination);
   },
 
-  'ReadStatement': function (s) {
+  ReadStatement(s) {
     // Call scanf from C lib, format string in rdi, operand in rsi
-    s.varexps.forEach(function (v) {
-      emit('mov', gen(v), '%rsi')
-      emit('lea', 'READ(%rip)', '%rdi')
-      emit('xor', '%rax', '%rax')
-      emit('call', '_scanf')
-    })
+    s.varexps.forEach((v) => {
+      emit('mov', gen(v), '%rsi');
+      emit('lea', 'READ(%rip)', '%rdi');
+      emit('xor', '%rax', '%rax');
+      emit('call', '_scanf');
+    });
   },
 
-  'WriteStatement': function (s) {
+  WriteStatement(s) {
     // Call printf from C lib, format string in rdi, operand in rsi, rax=0
-    s.expressions.forEach(function (e) {
-      emit('mov', gen(e), '%rsi')
-      emit('lea', 'WRITE(%rip)', '%rdi')
-      emit('xor', '%rax', '%rax')
-      emit('call', '_printf')
-    })
+    s.expressions.forEach((e) => {
+      emit('mov', gen(e), '%rsi');
+      emit('lea', 'WRITE(%rip)', '%rdi');
+      emit('xor', '%rax', '%rax');
+      emit('call', '_printf');
+    });
   },
 
-  'WhileStatement': function (s) {
+  WhileStatement(s) {
     const top = makeLabel();
     const bottom = makeLabel();
     emitLabel(top);
     const condition = gen(s.condition);
     emitJumpIfFalse(condition, bottom);
     allocator.freeAllRegisters();
-    gen(s.body)
+    gen(s.body);
     emit('jmp', top);
     emitLabel(bottom);
   },
 
-  'IntegerLiteral': function (literal) {
+  IntegerLiteral(literal) {
     return new ImmediateOperand(literal.toString());
   },
 
-  'BooleanLiteral': function (literal) {
-    return new ImmediateOperand(['false','true'].indexOf(literal.toString()))
+  BooleanLiteral(literal) {
+    return new ImmediateOperand(['false', 'true'].indexOf(literal.toString()));
   },
 
-  'VariableExpression': function (v) {
+  VariableExpression(v) {
     const name = makeVariable(v.referent);
     usedVariables[name] = true;
     return new MemoryOperand(name);
   },
 
-  'UnaryExpression': function (e) {
-    const result = allocator.ensureRegister(gen(e.operand))
-    const instruction = {'-':'neg', 'not':'not'}[e.op]
-    emit(instruction, result)
-    return result
+  UnaryExpression(e) {
+    const result = allocator.ensureRegister(gen(e.operand));
+    const instruction = { '-': 'neg', not: 'not' }[e.op];
+    emit(instruction, result);
+    return result;
   },
 
-  'BinaryExpression': function (e) {
-    const left = gen(e.left)
+  BinaryExpression(e) {
+    const left = gen(e.left);
     const result = (e.op === '/') ?
-      allocator.makeRegisterOperandFor("rax") :
-      allocator.ensureRegister(left)
+      allocator.makeRegisterOperandFor('rax') :
+      allocator.ensureRegister(left);
 
     if (e.op === 'and') {
-      emitShortCircuit('je', e, result)
-      return result
+      emitShortCircuit('je', e, result);
+      return result;
     }
 
     if (e.op === 'or') {
-      emitShortCircuit('jne', e, result)
-      return result
+      emitShortCircuit('jne', e, result);
+      return result;
     }
 
-    const right = gen(e.right)
+    const right = gen(e.right);
 
     if (e.op === '/') {
-      emit("movq", left, result);
-      emit("cqto");
-      emit("idivq", allocator.nonImmediate(right));
+      emit('movq', left, result);
+      emit('cqto');
+      emit('idivq', allocator.nonImmediate(right));
     } else {
       switch (e.op) {
-        case '+': emit("addq", right, result); break
-        case '-': emit("subq", right, result); break
-        case '*': emit("imulq", right, result); break
-        case '<': emitComparison("setl", right, result); break
-        case '<=': emitComparison("setle", right, result); break
-        case '==': emitComparison("sete", right, result); break
-        case '!=': emitComparison("setne", right, result); break
-        case '>=': emitComparison("setge", right, result); break
-        case '>': emitComparison("setg", right, result); break
+        case '+': emit('addq', right, result); break;
+        case '-': emit('subq', right, result); break;
+        case '*': emit('imulq', right, result); break;
+        case '<': emitComparison('setl', right, result); break;
+        case '<=': emitComparison('setle', right, result); break;
+        case '==': emitComparison('sete', right, result); break;
+        case '!=': emitComparison('setne', right, result); break;
+        case '>=': emitComparison('setge', right, result); break;
+        case '>': emitComparison('setg', right, result); break;
+        default: break;
       }
     }
     return result;
-  }
-}
+  },
+};
 
 function emitLabel(label) {
-  console.log(label + ':')
+  console.log(`${label}:`);
 }
 
 function emit(op, x, y) {
-  const line = '\t' + op
-  if (x) line += '\t' + x
-  if (y) line += ', ' + y
-  console.log(line)
+  let line = `\t${op}`;
+  if (x) line += `\t${x}`;
+  if (y) line += `, ${y}`;
+  console.log(line);
 }
 
 function emitShortCircuit(operation, expression, destination) {
-  const skip = makeLabel()
-  emit("cmp", "$0", destination)
-  emit(operation, skip)
-  const right = gen(expression.right)
-  emit("mov", right, destination)
-  emitLabel(skip)
+  const skip = makeLabel();
+  emit('cmp', '$0', destination);
+  emit(operation, skip);
+  const right = gen(expression.right);
+  emit('mov', right, destination);
+  emitLabel(skip);
 }
 
 function emitComparison(operation, right, destination) {
-  emit("cmp", right, destination)
-  const byteRegister = '%' + allocator.byteRegisterFor(destination.register)
-  emit(operation, byteRegister)
-  emit("movsbq", byteRegister, destination)
+  emit('cmp', right, destination);
+  const byteRegister = `%${allocator.byteRegisterFor(destination.register)}`;
+  emit(operation, byteRegister);
+  emit('movsbq', byteRegister, destination);
 }
 
 function emitJumpIfFalse(operand, label) {
@@ -192,112 +191,111 @@ function emitJumpIfFalse(operand, label) {
   // compare two immediate values, so if the operand is immediate we have to get a new
   // register for it.
 
-  emit('cmpq', '$0', allocator.nonImmediate(operand))
-  emit('je', label)
+  emit('cmpq', '$0', allocator.nonImmediate(operand));
+  emit('je', label);
 }
 
-
-function RegisterAllocator () {
+class RegisterAllocator {
   // A ridiculously simple register allocator. It throws an exception if there are no free
   // registers available.  Also, it never allocates %rdx, since that is used for division.
   // And it never allocates %rdi or %rsi, as those are used for reading and writing.  Also,
   // you can't mark individual registers free; you can only call freeAllRegisters().
 
-  this.names = ['rax','rcx','r8','r9','r10','r11']
-  this.bindings = new HashMap()
-}
-
-RegisterAllocator.prototype.byteRegisterFor = function (registerName) {
-  return ['al', 'cl', 'r8b', 'r9b', 'r10b', 'r11b'][this.names.indexOf(registerName)]
-}
-
-RegisterAllocator.prototype.makeRegisterOperand = function () {
-  // Returns a brand new register operand bound to the first available free register
-
-  const operand = new RegisterOperand("");
-  this.assignFreeRegisterTo(operand);
-  return operand;
-}
-
-RegisterAllocator.prototype.makeRegisterOperandFor = function (registerName) {
-  // Returns a brand new register operand bound to a specific register.  If something is
-  // already in that register, generates code to move it out and rebind to a new register.
-
-  const existingRegisterOperand = this.bindings.get(registerName);
-  if (existingRegisterOperand) {
-    this.assignFreeRegisterTo(existingRegisterOperand);
-    emit("movq", "%" + registerName, existingRegisterOperand);
+  constructor() {
+    this.names = ['rax', 'rcx', 'r8', 'r9', 'r10', 'r11'];
+    this.bindings = new HashMap();
   }
-  const operand = new RegisterOperand(registerName);
-  this.bindings.set(registerName, operand);
-  return operand;
-}
 
-RegisterAllocator.prototype.nonImmediate = function (operand) {
-  // If the operand is already non-immediate, return it, otherwise generate a new register
-  // operand containing this value.
-
-  if (operand instanceof ImmediateOperand) {
-    const newOperand = this.makeRegisterOperand();
-    emit("movq", operand + ", " + newOperand);
-    return newOperand;
+  byteRegisterFor(registerName) {
+    return ['al', 'cl', 'r8b', 'r9b', 'r10b', 'r11b'][this.names.indexOf(registerName)];
   }
-  return operand;
-}
 
-RegisterAllocator.prototype.ensureRegister = function (operand) {
-  // If the operand is already a register, return it, otherwise generate a new register
-  // operand containing this value.
-
-  if (! (operand instanceof RegisterOperand)) {
-    const newOperand = this.makeRegisterOperand();
-    emit("movq", operand, newOperand);
-    return newOperand;
+  makeRegisterOperand() {
+    // Returns a brand new register operand bound to the first available free register
+    const operand = new RegisterOperand('');
+    this.assignFreeRegisterTo(operand);
+    return operand;
   }
-  return operand;
-}
 
-RegisterAllocator.prototype.assignFreeRegisterTo = function (registerOperand) {
-  // Changes the register value of an existing register operand to the first available register.
+  makeRegisterOperandFor(registerName) {
+    // Returns a brand new register operand bound to a specific register.  If something is
+    // already in that register, generates code to move it out and rebind to a new register.
 
-  for (const i = 0; i < this.names.length; i++) {
-    const register = this.names[i]
-    if (!this.bindings.has(register)) {
-      this.bindings.set(register, registerOperand);
-      registerOperand.register = register;
-      return;
+    const existingRegisterOperand = this.bindings.get(registerName);
+    if (existingRegisterOperand) {
+      this.assignFreeRegisterTo(existingRegisterOperand);
+      emit('movq', `%${registerName}`, existingRegisterOperand);
     }
+    const operand = new RegisterOperand(registerName);
+    this.bindings.set(registerName, operand);
+    return operand;
   }
-  throw 'No more registers available';
+
+  nonImmediate(operand) {
+    // If the operand is already non-immediate, return it, otherwise generate a new register
+    // operand containing this value.
+
+    if (operand instanceof ImmediateOperand) {
+      const newOperand = this.makeRegisterOperand();
+      emit('movq', `${operand}, ${newOperand}`);
+      return newOperand;
+    }
+    return operand;
+  }
+
+  ensureRegister(operand) {
+    // If the operand is already a register, return it, otherwise generate a new register
+    // operand containing this value.
+
+    if (!(operand instanceof RegisterOperand)) {
+      const newOperand = this.makeRegisterOperand();
+      emit('movq', operand, newOperand);
+      return newOperand;
+    }
+    return operand;
+  }
+
+  assignFreeRegisterTo(registerOperand) {
+    // Changes the register value of an existing register operand to the first available register.
+
+    const register = this.names.find(r => !this.bindings.has(r));
+    if (!register) {
+      throw 'No more registers available';
+    }
+    this.bindings.set(register, registerOperand);
+    registerOperand.register = register; // eslint-disable-line no-param-reassign
+  }
+
+  freeAllRegisters() {
+    this.bindings.clear();
+  }
 }
 
-RegisterAllocator.prototype.freeAllRegisters = function () {
-  this.bindings.clear()
+const allocator = new RegisterAllocator();
+
+class ImmediateOperand {
+  constructor(value) {
+    this.value = value;
+  }
+  toString() {
+    return `$${this.value}`;
+  }
 }
 
-const allocator = new RegisterAllocator()
-
-
-function ImmediateOperand(value) {
-  this.value = value
+class RegisterOperand {
+  constructor(register) {
+    this.register = register;
+  }
+  toString() {
+    return `%${this.register}`;
+  }
 }
 
-ImmediateOperand.prototype.toString = function () {
-  return '$' + this.value
-}
-
-function RegisterOperand(register) {
-  this.register = register
-}
-
-RegisterOperand.prototype.toString = function () {
-  return '%' + this.register
-}
-
-function MemoryOperand(variable) {
-  this.variable = variable
-}
-
-MemoryOperand.prototype.toString = function () {
-  return this.variable + '(%rip)'
+class MemoryOperand {
+  constructor(variable) {
+    this.variable = variable;
+  }
+  toString() {
+    return `${this.variable}(%rip)`;
+  }
 }
